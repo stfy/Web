c52b4428c50d44bbfec5e75b9354b3776c443a74
Timofey Semenyuk <rustfy@gmail.com>
2021-10-04 21:56:51 +0300

    i18n

--- src/components/TokenDashboardNavigation.js
+++ src/components/TokenDashboardNavigation.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { dashboardTokens, tokenInfo } from "./data/tokens";
+import { useTranslation } from "react-i18next";
 
 function TokenDashboardNavigation() {
+  const { t } = useTranslation();
+
   return (
     <div className="breadcrumbs">
       {dashboardTokens.map((token) => (
@@ -11,7 +14,7 @@ function TokenDashboardNavigation() {
           className="breadcrumbs__item"
           to={`/dashboard/${token}`}
         >
-          <span>{tokenInfo[token].fullName || tokenInfo[token].name}</span>
+          <span>{t(`token.${tokenInfo[token].name}`)}</span>
           <span className="breadcrumbs__delimiter" />
         </NavLink>
       ))}
